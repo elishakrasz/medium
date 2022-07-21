f391ee74c02c1d5d28599c220202b52e448eadfa
Elisha Mallard <elishashestov@gmail.com>
2022-07-21 06:18:38 +0300

    additional properties

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import { sanityClient } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+const Home = ({ posts }: Props) => {
+  console.log('indians', posts)
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -15,6 +22,28 @@ const Home: NextPage = () => {
       <Banner />
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+  }`;
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
 
 export default Home
